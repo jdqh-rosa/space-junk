f8f3be62ed92cd2160dc83c2a97bbace6a9c89db
Judah Rosaria <judahboy@live.com>
2020-05-08 14:10:32 +0200

    added Orbit script to make it easier to add orbitting objects

---
+++ Assets/Custom Assets/Scripts/Orbit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public enum orbitAxis{
+    X,
+    Y,
+    Z
+};
+    public orbitAxis orbitAxi;
+    public Vector3 pivotPoint;
+    public GameObject pivotObject;
+    public float radius = 10;
+    public float rotationSpeed = 80;
+    public float heightSpeed = 1;
+    private Vector3 axis = Vector3.up;
+
+    void Start()
+    {
+        switch(orbitAxi){
+            case orbitAxis.X: axis = Vector3.right;
+            break;
+            case orbitAxis.Y: axis = Vector3.up;
+            break;
+            case orbitAxis.Z: axis = Vector3.forward;
+            break;
+        }
+
+
+        Vector3 center;
+        if(pivotObject==null){
+            center = pivotPoint;
+        }else{
+            center = pivotObject.transform.position;
+        }
+
+        transform.position = (transform.position - center).normalized * radius + center;
+    }
+    void Update()
+    {
+        if(pivotObject==null){
+            OrbitAroundPoint(pivotPoint);
+        }else{OrbitAroundPoint(pivotObject.transform.position);}
+    }
+
+    private void OrbitAroundPoint(Vector3 pPoint)
+    {
+        transform.RotateAround(pPoint, axis, rotationSpeed * Time.deltaTime);
+        Vector3 goToPosition = (transform.position - pPoint).normalized * radius+pPoint;
+        transform.position = Vector3.MoveTowards(transform.position, goToPosition, Time.deltaTime * heightSpeed);
+    }
+}
