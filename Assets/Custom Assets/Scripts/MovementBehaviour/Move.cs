36ed344c2c8d646dbb0d7d21f21cb7d9944c2a8f
Judah Rosaria <judahboy@live.com>
2020-05-08 15:16:21 +0200

    made move script (contains ability to follow target)

---
+++ Assets/Custom Assets/Scripts/MovementBehaviour/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public enum moveAxis{
+    Forward,
+    Up,
+    Right
+    };
+
+    public moveAxis moveDirection;
+    public float movementSpeed=5;
+    public GameObject target;
+    private Vector3 direction;
+
+
+    void Start()
+    {
+        switch(moveDirection){
+            case moveAxis.Forward: direction = Vector3.forward;
+            break;
+            case moveAxis.Right: direction = Vector3.right;
+            break;
+            case moveAxis.Up: direction = Vector3.up;
+            break;
+        }
+    }
+    void Update()
+    {
+        if(!target){
+        transform.position += direction * movementSpeed;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+        }
+    }
+}
