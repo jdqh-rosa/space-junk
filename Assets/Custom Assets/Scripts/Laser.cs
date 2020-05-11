308b7540990226701754388e38cff365bebd053f
Judah Rosaria <judahboy@live.com>
2020-05-11 11:58:09 +0200

    -added laser Script (no functionality)

---
+++ Assets/Custom Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+     LineRenderer lr;
+     
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetWidth(.2f,.2f);
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit. collider)
+                {
+                    lr.SetPosition(1, hit.point);
+                }
+            }
+        else {
+        lr.SetPosition(1, transform.position + (transform.forward * 5000));
+        }
+    }
+}
---
+++ Assets/Custom Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+    public GameObject laserPrefab;
+    
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(laserPrefab);
+        }
+    }
+}
