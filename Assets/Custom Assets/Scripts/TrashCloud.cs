26a3a257a79da21e315665e2349eb5fb13cf8e04
Judah Rosaria <judahboy@live.com>
2020-05-12 13:48:13 +0200

    -Moved general methods to helper class -Added basic spacejunk controls

--- Assets/Custom Assets/Scripts/TrashCloud.cs
+++ Assets/Custom Assets/Scripts/TrashCloud.cs
@@ -14,4 +14,22 @@ public struct TrashCloud
     void Start(){
         debrisCloud = new GameObject[cloudSize];
     }
+
+//initialize the cloud objects in an array
+    public void CreateDebrisCloud(){
+        debrisCloud = new GameObject[cloudSize];
+        for(int i=0; i< cloudSize; ++i){
+            //Vector2 blah = Helper.CalculateDegPos(relRange.x + (range/(cloudSize-1))*i, radius);
+            debrisCloud[i] = debris as GameObject;
+        }
+    }
+
+    //Update the position of every cloud object
+    public void UpdatePos()
+    {
+        for(int i=0; i< cloudSize; ++i){
+            Vector2 blah = Helper.CalculateDegPos(relRange.x + (range/(cloudSize-1))*i, radius);
+            debrisCloud[i].transform.position = new Vector3(blah.x, blah.y, 0);
+        }
+    }
 }
