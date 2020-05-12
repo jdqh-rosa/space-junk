c834eb85375fad2e6e13a0a6fa29fa8abedbf940
Justin Buhrmann <justinbuhrmann1997@gmail.com>
2020-05-12 12:38:12 +0200

    Added no interaction reset script

---
+++ Assets/Custom Assets/Scripts/NoInteractionReset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoInteractionReset : MonoBehaviour
+{
+    public float ResetTimer = 30f;
+    private float timeleft;
+
+    private void Start()
+    {
+        //set initial time
+        timeleft = ResetTimer;
+    }
+
+    void Update()
+    {
+        //Check if any input is received during this frame
+        if(Input.anyKey)
+        {
+            //Reset the timer
+            timeleft = ResetTimer;
+        } else
+        {
+            //Subtract time from timer
+            timeleft -= Time.deltaTime;
+
+            //If no time is left, reset the game
+            if (timeleft <= 0f)
+            {
+                Debug.Log("RESET GAME");
+            }
+        }
+    }
+}
