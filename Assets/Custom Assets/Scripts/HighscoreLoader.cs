f8d0c38a31ee74c444c250ce0520c0cb66dfca53
Justin Buhrmann <justinbuhrmann1997@gmail.com>
2020-05-20 16:02:24 +0200

    Added highscore screen , Database, Data class and db connection class.

---
+++ Assets/Custom Assets/Scripts/HighscoreLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighscoreLoader : MonoBehaviour
+{
+    public GameObject RowPrefab;
+
+    private float rowheight;
+    private float currentY = 0;
+    void Start()
+    {
+        //Set rowheight to calculate height of other objects
+        rowheight = RowPrefab.GetComponent<RectTransform>().sizeDelta.y;
+
+        //Get all of the highscores
+        dbConnection db = new dbConnection();
+        List<highscore> scores = db.Select("SELECT * FROM highscores ORDER BY Score DESC");
+
+        //Loop trough all scores
+        int pos = 1;
+        foreach(highscore score in scores)
+        {
+            createRow(score, pos);
+            pos++;
+        }
+
+        //Set the size of the content
+        GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x, rowheight * scores.Count);
+    }
+
+    /// <summary>
+    /// This function creates a row, based on the given highscore and position
+    /// </summary>
+    /// <param name="pScore">highscore object with values</param>
+    /// <param name="position">position value</param>
+    private void createRow(highscore pScore, int position)
+    {
+        //Set position values
+        GameObject row = Instantiate(RowPrefab, transform, false);
+        row.GetComponent<RectTransform>().localPosition = new Vector3(0,currentY,0);
+        currentY -= rowheight;
+
+        //Set property values
+        row.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText("#"+position);
+        row.transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText(pScore.Name);
+        row.transform.GetChild(2).GetComponent<TextMeshProUGUI>().SetText(pScore.Score.ToString());
+        row.transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText(pScore.Date.ToShortDateString());
+    }
+}
--- Assets/Custom Assets/Scripts/dbConnection.cs
+++ Assets/Custom Assets/Scripts/dbConnection.cs
@@ -2,16 +2,68 @@
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
-using Microsoft.Data.Sqlite;
+using Mono.Data.Sqlite;
+using System.IO;
 
 /// <summary>
 /// Database helper class to easily read, modify and delete records from the database
 /// </summary>
-public class dbConnection {
-
-    private SqliteConnection connection;
+public class dbConnection
+{
+    private string connectionString, query;
+    private IDbConnection connection;
+    private IDbCommand command;
+    private IDataReader reader;
     public dbConnection()
     {
-        connection = new SqliteConnection("Filename="+Application.dataPath+"Custom Assets/database.db");
+        connectionString = "URI=file:" + Application.dataPath + "/Custom Assets/database.db";
+
+        Debug.Log("Stablishing connection to: " + connectionString);
+        connection = new SqliteConnection(connectionString);
+        connection.Open();
+    }
+
+    /// <summary>
+    /// Select function to select highscores
+    /// </summary>
+    /// <param name="Query">Query to be executed by sqlite</param>
+    /// <returns>A list of highscores returned by the query</returns>
+    public List<highscore> Select(string pQuery)
+    {
+        //Create the list
+        List<highscore> highScores = new List<highscore>();
+
+        //Create the sql command
+        command = connection.CreateCommand();
+        //Set command text
+        command.CommandText = pQuery;
+        //Execute the sql command
+        IDataReader reader = command.ExecuteReader();
+
+        //This while loops over all found entries (1 loop for each found entry)
+        while (reader.Read())
+        {
+            //Get the entry values from the reader
+            int id = reader.GetInt32(0);
+            int score = reader.GetInt32(1);
+            System.DateTime date = reader.GetDateTime(2);
+            string name = reader.GetString(3);
+
+            //Add the entry to the output list
+            highScores.Add(new highscore(id, score, date, name));
+        }
+
+        //Close the reader
+        reader.Close();
+        //Dispose the command
+        command.Dispose();
+
+        //Return the result(s)
+        return highScores;
+    }
+
+    public void InsertHighscore(highscore pScore)
+    {
+
     }
 }
