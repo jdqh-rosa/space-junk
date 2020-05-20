f7b65ecc4aaddcea3e0cc2f5f98c35b31af170eb
Justin Buhrmann <justinbuhrmann1997@gmail.com>
2020-05-20 13:35:44 +0200

    added db script

---
+++ Assets/Custom Assets/Scripts/dbConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using Microsoft.Data.Sqlite;
+
+/// <summary>
+/// Database helper class to easily read, modify and delete records from the database
+/// </summary>
+public class dbConnection {
+
+    private SqliteConnection connection;
+    public dbConnection()
+    {
+        connection = new SqliteConnection("Filename="+Application.dataPath+"Custom Assets/database.db");
+    }
+}
