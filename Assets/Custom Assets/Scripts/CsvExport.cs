eee04289b23c8e12263b273f563a2b1c8a0aab54
Justin Buhrmann <justinbuhrmann1997@gmail.com>
2020-05-26 15:47:50 +0200

    Added a .csv export script

---
+++ Assets/Custom Assets/Scripts/CsvExport.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+public class CsvExport : MonoBehaviour
+{
+    public void Export()
+    {
+        List<string[]> rowData = new List<string[]>();
+
+        //Add the date to the file
+        string[] rowDataTemp = new string[1];
+        rowDataTemp[0] = "Highscores van mei 2020";
+        rowData.Add(rowDataTemp);
+
+        // Create the row headers
+        rowDataTemp = new string[4];
+        rowDataTemp[0] = "POSITIE";
+        rowDataTemp[1] = "NAAM";
+        rowDataTemp[2] = "SCORE";
+        rowDataTemp[3] = "DATUM";
+        rowData.Add(rowDataTemp);
+
+        // Create a database connection
+        dbConnection db = new dbConnection();
+        List<highscore> scores = db.Select("SELECT * FROM highscores ORDER BY Score DESC");
+
+        // Loop trough highscores and add them to new rows
+        int pos = 1;
+        foreach (highscore score in scores)
+        {
+            rowDataTemp = new string[4];
+            rowDataTemp[0] = "#"+pos;
+            rowDataTemp[1] = score.Name;
+            rowDataTemp[2] = ""+ score.Score;
+            rowDataTemp[3] = "" + score.Date.ToString("dd/MM/yyyy");
+            rowData.Add(rowDataTemp);
+            pos++;
+        }
+
+        string[][] output = new string[rowData.Count][];
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = rowData[i];
+        }
+
+        int length = output.GetLength(0);
+        string delimiter = "|";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+
+        string filePath = Application.dataPath + "/highscore_export.csv";
+
+        StreamWriter outStream = File.CreateText(filePath);
+        outStream.WriteLine("sep=|");
+        outStream.WriteLine(sb);
+        outStream.Close();
+    }
+}
