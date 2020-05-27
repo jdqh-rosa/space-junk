e4282b885d06acf7bca7e3495afeb1627dcfa9ea
Justin Buhrmann <justinbuhrmann1997@gmail.com>
2020-05-27 16:11:57 +0200

    added a result screen and an onscreen keyboard

---
+++ Assets/Custom Assets/Scripts/ScreenKeyboard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScreenKeyboard : MonoBehaviour
+{
+    public TMP_InputField Input;
+    public TextMeshProUGUI CountText;
+    public int MaxLength;
+
+    private void Start()
+    {
+        CountText.SetText("0 / " + MaxLength);
+    }
+
+    public void KeyPress(string key)
+    {
+        if (Input != null)
+        {
+            switch(key)
+            {
+                case "backspace":
+                    if(Input.text != "")
+                    {
+                        Input.text = Input.text.Remove(Input.text.Length - 1);
+                    }
+                    break;
+                case "enter":
+                    slideOut();
+                    break;
+                default:
+                    if(Input.text.Length < MaxLength)
+                    {
+                        Input.text += key;
+                    }
+                    break;
+            }
+
+            if(CountText != null)
+            {
+                CountText.SetText(Input.text.Length + " / " + MaxLength);
+            }
+        }
+    }
+
+    private void slideOut()
+    {
+        GetComponent<Animator>().SetBool("Close", true);
+    }
+}
